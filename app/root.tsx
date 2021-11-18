d5b61ecea131c833e6503cbe5adebbf3fabbf5a8
Edmund Hung <me@edmund.dev>
2021-11-18 01:36:01 +0100

    make the search bar responsive for ipad

--- app/root.tsx
+++ app/root.tsx
@@ -67,87 +67,90 @@ export default function App() {
           >
             <span className="text-center">Remix Guide</span>
           </Link>
-          <Form
-            className="px-10 relative flex-grow color-gray-300"
-            method="get"
-          >
-            <div class="flex items-center flex-row-reverse">
-              <input
-                id="search"
-                className="w-full pr-4 pl-9 py-2 text-gray-700 border-b focus:outline-none focus:border-gray-700 appearance-none"
-                type="search"
-                name="q"
-                placeholder="Search"
-              />
-              <label htmlFor="search" className="-mr-7">
-                <svg
-                  class="w-5 h-5 fill-current text-gray-500 z-10"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="black"
+          <div className="px-10 flex flex-grow flex-col lg:flex-row">
+            <Form className="relative flex-grow color-gray-300" method="get">
+              <div class="flex items-center flex-row-reverse">
+                <input
+                  id="search"
+                  className="w-full pr-4 pl-9 py-2 text-gray-700 border-b focus:outline-none focus:border-gray-700 appearance-none"
+                  type="search"
+                  name="q"
+                  placeholder="Search"
+                />
+                <label htmlFor="search" className="-mr-7">
+                  <svg
+                    class="w-5 h-5 fill-current text-gray-500 z-10"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="black"
+                  >
+                    <path d="M0 0h24v24H0V0z" fill="none" />
+                    <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z" />
+                  </svg>
+                </label>
+              </div>
+            </Form>
+            <div className="flex flex-col sm:flex-row items-center pt-2">
+              <div className="flex flex-row lg:px-10">
+                <Form method="get">
+                  <select className="px-2 text-gray-300 hover:text-gray-600 transition-colors cursor-pointer appearance-none">
+                    <option>Version</option>
+                  </select>
+                </Form>
+                <Form method="get">
+                  <select className="px-2 text-gray-300 hover:text-gray-600 transition-colors cursor-pointer appearance-none">
+                    <option>Platform</option>
+                  </select>
+                </Form>
+              </div>
+              <nav className="flex-grow sm:text-right">
+                <NavLink
+                  className={({ isActive }) =>
+                    `px-2 ${
+                      isActive ? 'text-gray-900' : 'text-gray-300'
+                    } hover:text-gray-600 transition-colors`
+                  }
+                  to="articles"
+                  prefetch="intent"
                 >
-                  <path d="M0 0h24v24H0V0z" fill="none" />
-                  <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z" />
-                </svg>
-              </label>
+                  <span>Articles</span>
+                </NavLink>
+                <NavLink
+                  className={({ isActive }) =>
+                    `px-2 ${
+                      isActive ? 'text-gray-900' : 'text-gray-300'
+                    } hover:text-gray-600 transition-colors`
+                  }
+                  to="packages"
+                  prefetch="intent"
+                >
+                  <span>Packages</span>
+                </NavLink>
+                <NavLink
+                  className={({ isActive }) =>
+                    `px-2 ${
+                      isActive ? 'text-gray-900' : 'text-gray-300'
+                    } hover:text-gray-600 transition-colors`
+                  }
+                  to="templates"
+                  prefetch="intent"
+                >
+                  <span>Templates</span>
+                </NavLink>
+                <NavLink
+                  className={({ isActive }) =>
+                    `px-2 ${
+                      isActive ? 'text-gray-900' : 'text-gray-300'
+                    } hover:text-gray-600 transition-colors`
+                  }
+                  to="examples"
+                  prefetch="intent"
+                >
+                  <span>Examples</span>
+                </NavLink>
+              </nav>
             </div>
-          </Form>
-          <Form method="get">
-            <select className="px-2 text-gray-300 hover:text-gray-600 transition-colors cursor-pointer appearance-none">
-              <option>Version</option>
-            </select>
-          </Form>
-          <Form method="get">
-            <select className="px-2 text-gray-300 hover:text-gray-600 transition-colors cursor-pointer appearance-none">
-              <option>Platform</option>
-            </select>
-          </Form>
-          <nav className="px-10">
-            <NavLink
-              className={({ isActive }) =>
-                `px-2 ${
-                  isActive ? 'text-gray-900' : 'text-gray-300'
-                } hover:text-gray-600 transition-colors`
-              }
-              to="articles"
-              prefetch="intent"
-            >
-              <span>Articles</span>
-            </NavLink>
-            <NavLink
-              className={({ isActive }) =>
-                `px-2 ${
-                  isActive ? 'text-gray-900' : 'text-gray-300'
-                } hover:text-gray-600 transition-colors`
-              }
-              to="packages"
-              prefetch="intent"
-            >
-              <span>Packages</span>
-            </NavLink>
-            <NavLink
-              className={({ isActive }) =>
-                `px-2 ${
-                  isActive ? 'text-gray-900' : 'text-gray-300'
-                } hover:text-gray-600 transition-colors`
-              }
-              to="templates"
-              prefetch="intent"
-            >
-              <span>Templates</span>
-            </NavLink>
-            <NavLink
-              className={({ isActive }) =>
-                `px-2 ${
-                  isActive ? 'text-gray-900' : 'text-gray-300'
-                } hover:text-gray-600 transition-colors`
-              }
-              to="examples"
-              prefetch="intent"
-            >
-              <span>Examples</span>
-            </NavLink>
-          </nav>
+          </div>
         </header>
         <Outlet />
         <footer className="sm:px-10 p-5">
