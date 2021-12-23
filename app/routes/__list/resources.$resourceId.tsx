a912797cb3459fbfd129f47012e5aab01f773a1a
Edmund Hung <me@edmund.dev>
2021-12-23 20:56:34 +0100

    feat: use button value for type field

--- app/routes/__list/resources.$resourceId.tsx
+++ app/routes/__list/resources.$resourceId.tsx
@@ -61,11 +61,13 @@ export let action: ActionFunction = async ({ context, params, request }) => {
   switch (type) {
     case 'bookmark':
       await store.bookmark(profile.id, params.resourceId ?? '');
-      return redirect(request.headers.get('referrer') ?? request.url);
+      break;
     case 'unbookmark':
       await store.unbookmark(profile.id, params.resourceId ?? '');
-      return redirect(request.headers.get('referrer') ?? request.url);
+      break;
   }
+
+  return redirect(request.headers.get('referrer') ?? request.url);
 };
 
 export let loader: LoaderFunction = async ({ context, params }) => {
@@ -179,13 +181,10 @@ export default function EntryDetail() {
       message={message}
       elements={
         <Form className="flex flex-row items-center" method="post">
-          <input
-            type="hidden"
-            name="type"
-            value={bookmarked ? 'unbookmark' : 'bookmark'}
-          />
           <button
             type="submit"
+            name="type"
+            value={bookmarked ? 'unbookmark' : 'bookmark'}
             className={`flex items-center justify-center w-6 h-6 ${
               bookmarked
                 ? 'rounded-full text-red-500 bg-gray-200'
