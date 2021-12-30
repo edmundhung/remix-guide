4b9f81ce6bee728d20ea1386bba9b18f62a58a86
Edmund Hung <me@edmund.dev>
2021-12-30 18:38:46 +0100

    test: ensure logout works

--- cypress/integration/authentication.spec.ts
+++ cypress/integration/authentication.spec.ts
@@ -1,23 +1,24 @@
 describe('Authentication', () => {
   beforeEach(() => {
-    cy.visit('/?menu=open');
+    cy.visit('/');
   });
 
-  it('redirect users to github when login', () => {
-    cy.intercept(/^https:\/\/github.com\/login\/oauth\/authorize/, (req) => {
-      const url = new URL(req.url);
-      const redirectUri = url.searchParams.get('redirect_uri');
+  it('shows a Logout button after login', () => {
+    cy.login();
 
-      req.redirect(
-        `${redirectUri}?code=remix-guide&state=${url.searchParams.get('state')}`
-      );
-    }).as('login');
-
-    cy.findByText(/Login with GitHub/i).click();
-    cy.wait('@login');
     cy.visit('/?menu=open');
-
     cy.findByText(/Login with GitHub/i).should('not.exist');
     cy.findByText(/Logout/i).should('exist');
+  });
+
+  it('allows user to logout', () => {
+    cy.login();
+
+    cy.visit('/?menu=open');
+    cy.findByText(/Logout/i).click();
+
+    cy.visit('/?menu=open');
+    cy.findByText(/Login with GitHub/i).should('exist');
+    cy.findByText(/Logout/i).should('not.exist');
   });
 });
