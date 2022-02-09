bef3db7ade72a08c475a1445c5130f84c286155d
Edmund Hung <me@edmund.dev>
2022-02-09 23:28:13 +0100

    chore: remove fathom

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,13 +1,4 @@
 import ReactDOM from 'react-dom';
 import { RemixBrowser } from 'remix';
-import { load } from 'fathom-client';
 
 ReactDOM.hydrate(<RemixBrowser />, document);
-
-if (process.env.NODE_ENV === 'production') {
-	load('AVOQUSXG', {
-		url: 'https://cdn.remix.guide/script.js',
-		spa: 'history',
-		excludedDomains: ['localhost'],
-	});
-}
