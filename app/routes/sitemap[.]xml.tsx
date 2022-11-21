c91c08c204cce9f94841bb0986a9c06be9697c46
Edmund Hung <me@edmund.dev>
2022-11-21 12:53:06 +0100

    feat: setup sitemap

---
+++ app/routes/sitemap[.]xml.tsx
@@ -0,0 +1,71 @@
+import type { LoaderArgs } from '@remix-run/cloudflare';
+
+interface SitemapEntry {
+	loc: string;
+	lastmod?: string;
+	changefreq?:
+		| 'never'
+		| 'yearly'
+		| 'monthly'
+		| 'weekly'
+		| 'daily'
+		| 'hourly'
+		| 'always';
+	priority?: 1.0 | 0.9 | 0.8 | 0.7 | 0.6 | 0.5 | 0.4 | 0.3 | 0.2 | 0.1 | 0.0;
+}
+
+export async function loader({ context }: LoaderArgs) {
+	function formatEntry(entry: SitemapEntry) {
+		return `
+            <url>
+                <loc>${entry.loc}</loc>
+                ${entry.lastmod ? `<lastmod>${entry.lastmod}</lastmod>` : ''}
+                ${
+									entry.changefreq
+										? `<changefreq>${entry.changefreq}</changefreq>`
+										: ''
+								}
+                ${
+									entry.priority ? `<priority>${entry.priority}</priority>` : ''
+								}
+            </url>
+        `.trim();
+	}
+
+	const guide = await context.resourceStore.getData();
+	const domain = 'https://remix.guide';
+	const entries: SitemapEntry[] = [
+		{ loc: domain, changefreq: 'hourly', priority: 1 },
+	];
+
+	for (const list of guide.metadata.lists ?? []) {
+		entries.push({
+			loc: `${domain}/${list.slug}`,
+			changefreq: 'hourly',
+			priority: 0.8,
+		});
+	}
+
+	for (const [resourceId, resource] of Object.entries(guide.value)) {
+		entries.push({
+			loc: `${domain}/resources/${resourceId}`,
+			lastmod: resource.updatedAt,
+			changefreq: 'weekly',
+			priority: 0.5,
+		});
+	}
+
+	const sitemap = `
+        <?xml version="1.0" encoding="UTF-8"?>
+        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+            ${entries.map(formatEntry)}
+        </urlset>
+    `.trim();
+
+	return new Response(sitemap, {
+		headers: {
+			'Content-Type': 'application/xml',
+			'Content-Length': String(new TextEncoder().encode(sitemap).length),
+		},
+	});
+}
