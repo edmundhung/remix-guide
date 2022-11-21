eabc4e89d5067e3e52c3500fc2de2225bd8c6644
Edmund Hung <me@edmund.dev>
2022-11-21 20:27:40 +0100

    fix: ensure graceful migration from the old urls

--- app/routes/discover.tsx
+++ app/routes/discover.tsx
@@ -2,7 +2,18 @@ import type { LoaderArgs } from '@remix-run/cloudflare';
 import { redirect } from '@remix-run/cloudflare';
 
 export function loader({ request }: LoaderArgs) {
-	const url = new URL(request.url);
+	const { searchParams } = new URL(request.url);
+	const resourceId = searchParams.get('resourceId');
 
-	throw redirect(`/${url.search}`, 301);
+	searchParams.delete('resourceId');
+
+	const search =
+		Array.from(searchParams.keys()).length > 0
+			? `?${searchParams.toString()}`
+			: '';
+	const url = resourceId
+		? `/resources/${resourceId}${search}`
+		: `/resources${search}`;
+
+	throw redirect(url, 301);
 }
