71b8841bc0f6df6fe0dc8f992eb3a33ff2ae0602
Edmund Hung <me@edmund.dev>
2022-11-21 18:09:56 +0100

    fix: missing index meta

--- app/routes/_layout.index.tsx
+++ app/routes/_layout.index.tsx
@@ -1,8 +1,9 @@
-import type { LoaderArgs } from '@remix-run/cloudflare';
+import type { LoaderArgs, MetaFunction } from '@remix-run/cloudflare';
 import { json } from '@remix-run/cloudflare';
 import { useLoaderData } from '@remix-run/react';
 import About from '~/components/About';
 import Feed from '~/components/Feed';
+import { formatMeta } from '~/helpers';
 import { search } from '~/resources';
 import { getSearchOptions } from '~/search';
 
@@ -12,6 +13,14 @@ export async function loader({ request, context }: LoaderArgs) {
 
 	return json(search(list, searchOptions));
 }
+
+export const meta: MetaFunction<typeof loader> = () => {
+	return formatMeta({
+		title: 'Remix Guide',
+		description: 'A platform for the Remix community',
+		'og:url': `https://remix.guide`,
+	});
+};
 
 export default function Index() {
 	const { entries, count } = useLoaderData<typeof loader>();
